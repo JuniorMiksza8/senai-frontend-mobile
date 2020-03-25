4fed748cb0e3894b0b9368bb4ea2b968f36e8824
Junior Miksza <junior.miksza8@gmail.com>
2020-03-25 11:51:00 -0300

    Categorias e disponiblidade na pagina de frota

---
+++ src/models/veiculo.ts
@@ -0,0 +1,8 @@
+export interface Veiculo{
+    placa : string,
+    marca : string,
+    modelo : string,
+    km? : number,
+    tipo? : string
+    categoria? : string
+}
--- src/pages/carros/carros.ts
+++ src/pages/carros/carros.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Veiculo } from '../../models/veiculo';
+import { Categoria } from '../../models/categoria';
 
-/**
- * Generated class for the CarrosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +11,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CarrosPage {
 
+  Items : Array<Veiculo> = [];
+  categorias : Array<Categoria> = [];
+  disponibilidade : string = '1';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
+    this.initializeCategorias();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CarrosPage');
+    
+  }
+
+  initializeItems(){
+    let obj = {placa : 'QXE-9399',marca : 'Fiat',modelo : 'Uno',categoria : 'Carros'};
+    this.Items = [
+      obj
+    ];
+  }
+
+  initializeCategorias(){
+    let obj1 = {id : 0,nome : 'Carros'};
+    let obj2 = {id : 1,nome : 'Motos'};
+    this.categorias = [
+      obj1,
+      obj2
+    ];
+  }
+
+  getCarsByCategoria(categoria : string){
+    let items : Array<Veiculo> = this.Items; 
+    let itemsOfCategoria : Array<Veiculo> = [];
+    items.forEach(function(value){
+      if(value.categoria.toLowerCase() == categoria.toLowerCase()){
+        itemsOfCategoria.push(value);       
+      }    
+    });
+    return itemsOfCategoria;
+  }   
+
+  setDisponiblidade(){
+    
   }
 
 }
